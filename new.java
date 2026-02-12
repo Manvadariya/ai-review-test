5a6849bfe8e6bdaac2bd1a4872d826c69e6d9006
Manvadariya <manvadariya2@gmail.com>
2026-02-13 01:51:18 +0530

    testting_final

--- new.java
+++ new.java
@@ -5,4 +5,7 @@ public class New {
     static void opt(String message) {
         System.out.println(message);
     }
+    static void println(String message) {
+        System.out.println(message);
+    }
 }
